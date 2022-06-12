0e99c1859629e23689e475d4a58d2391d821acf5
henrycky <henrycky_wottikosky@hotmail.com>
2022-06-12 17:15:48 -0300

    Mais ferramentas

---
+++ src/database/migrations/1655063424201-CreateCategories.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategories1655063424201 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
