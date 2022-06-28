daf8d9699d880d35efc92c6ae007db4f279aaaaa
JuliaCald <j.caldeira.j@gmail.com>
2022-06-27 23:23:27 -0300

    Adição do arquivo de importação para testagem!

--- src/modules/pizzas/repositories/implementations/OrdersRepository.ts
+++ src/modules/pizzas/repositories/implementations/OrdersRepository.ts
@@ -17,32 +17,31 @@ class OrdersRepository implements IOrdersRepository {
 
     async create({ clientName, sauce, meat, crust, size, totalPrice }: ICreateOrderDTO): Promise<void> {
         
-
-        const isThereTheSauce = await this.repository.query('SELECT * FROM "sauces" where sauces.name ILIKE $1', [sauce]);
-        if (!isThereTheSauce){
-            throw new Error("Sorry! Sauce not available today, please try again tomorrow");
-        }
-
+        //captura o preço da sauce da order para acrescentar ao totalPrice da order
         const price1 =  await this.repository.query('SELECT price FROM "sauces" where sauces.name ILIKE $1', [sauce]);
         var s1 = JSON.stringify(price1);
         var d1 = JSON.parse(s1)[0].price;
         //console.log(d1);
 
+        //captura o preço da meat da order para acrescentar ao totalPrice da order
         const price2 =  await this.repository.query('SELECT price FROM "meats" where meats.name ILIKE $1', [meat]);
         var s2 = JSON.stringify(price2);
         var d2 = JSON.parse(s2)[0].price;
         //console.log(d2);
 
+        //captura o preço da sauce da crust para acrescentar ao totalPrice da order
         const price3 =  await this.repository.query('SELECT price FROM "crusts" where crusts.name ILIKE $1', [crust]);
         var s3 = JSON.stringify(price3);
         var d3 = JSON.parse(s3)[0].price;
         //console.log(d3);
 
+        //captura o preço do size da order para acrescentar ao totalPrice da order
         const price4 =  await this.repository.query('SELECT price FROM "sizes" where sizes.name ILIKE $1', [size]);
         var s4 = JSON.stringify(price4);
         var d4 = JSON.parse(s4)[0].price;
         //console.log(d4);
 
+        //soma o valor de cada atributo da pizza escolhidos na order para dar o valor final
         const total = d4 + d3 + d2 + d1;
         console.log(total);
 
